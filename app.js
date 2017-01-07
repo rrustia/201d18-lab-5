ea94306a96c4480e8019a247117f9d358c872f69
Ronel Rustia <fedstats@yahoo.com>
2017-01-06 17:06:09 -0800

    sumArray finction done!

--- app.js
+++ app.js
@@ -84,12 +84,19 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2,3,4]; //eslint-disable-line
 
 function sumArray(testArray){ //eslint-disable-line
-
+  var sumTemp = [];
+  sumTemp[0] = 0;
+  var number = 0;
+  var counter = testArray.length;
+  for (var index = 0; index < counter; index++) {
+    sumTemp = sum(sumTemp[0],testArray[index]);
+  }
+  return [sumTemp[0], testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + sumTemp[0] + ' is their sum.'];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
